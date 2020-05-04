e4adc1e8ce5348194f146fbdaceebec3226a0960
yoshiki <ev422119.s.okayama@gmail.com>
2020-05-04 20:57:19 +0900

    下タブ以外のfragmentは再生成しないようにCustomNavigator修正

--- app/src/main/java/com/temlete/presentation/common/CustomNavigator.kt
+++ app/src/main/java/com/temlete/presentation/common/CustomNavigator.kt
@@ -8,6 +8,9 @@ import androidx.navigation.NavOptions
 import androidx.navigation.Navigator
 import androidx.navigation.fragment.FragmentNavigator
 import androidx.navigation.fragment.NavHostFragment
+import com.temlete.presentation.dashboard.DashboardFragment
+import com.temlete.presentation.home.HomeFragment
+import com.temlete.presentation.notifications.NotificationsFragment
 
 /** Navigationでfragmentを切り替える際のカスタムナビゲーター
  *  切り替え時にfragmentを再生成しない
@@ -44,11 +47,24 @@ class CustomNavigator(
             transaction.hide(currentFragment)
         }
 
+        // BottomNavigationのfragmentでないことを表すフラグ
+        var isNotBottomNavigation = true
+        BottomNavigationFragment.values().forEach { bottomNavigationList ->
+            if (destination.className == bottomNavigationList.className) {
+                isNotBottomNavigation = false
+            }
+        }
+
         var fragment = manager.findFragmentByTag(tag)
-        if (fragment == null) {
+
+        // fragment初期作成時 or BottomNavigationのfragmentでない場合
+        if (fragment == null || isNotBottomNavigation) {
             fragment = instantiateFragment(context, manager, className, args)
+
+            // fragmentを生成
             transaction.add(containerId, fragment, tag)
         }
+
         fragment.arguments = args
 
         transaction.show(fragment)
@@ -63,4 +79,14 @@ class CustomNavHostFragment : NavHostFragment() {
     override fun createFragmentNavigator(): Navigator<out FragmentNavigator.Destination> {
         return CustomNavigator(requireContext(), childFragmentManager, id)
     }
+}
+
+/** BottomNavigationのFragment一覧
+ *  この一覧に追加したFragmentは、再表示した時にFragmentが再生成されない
+ *  BottomNavigation(下タブメニュー)を切り替えた時に、再ロードしないための実装
+ **/
+enum class BottomNavigationFragment(val className: String) {
+    HOME(HomeFragment::class.java.name),
+    DASHBOARD(DashboardFragment::class.java.name),
+    NOTIFICATION(NotificationsFragment::class.java.name)
 }
